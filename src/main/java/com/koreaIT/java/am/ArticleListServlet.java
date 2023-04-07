5dbca23772c84dd4c2f016328b67f02420ea7ea3
moebius8800 <moebius8800@gmail.com>
2023-04-07 15:02:03 +0900

    페이징 노출 갯수 제한

--- src/main/java/com/koreaIT/java/am/ArticleListServlet.java
+++ src/main/java/com/koreaIT/java/am/ArticleListServlet.java
@@ -44,6 +44,16 @@ public class ArticleListServlet extends HttpServlet {
 			int totalCount =  DBUtil.selectRowIntValue(conn, sql);
 			int totalPage = (int) Math.ceil((double) totalCount / itemsInAPage);
 			
+			int pageSize = 5;
+			int from = page - pageSize;
+			if (from < 1) {
+				from = 1;
+			}
+			int end = page + pageSize;
+			if (end > totalPage) {
+				end = totalPage;
+			}
+			
 			sql = SecSql.from("SELECT * FROM article");
 			sql.append("ORDER BY id DESC");
 			sql.append("LIMIT ?, ?", limitFrom, itemsInAPage);
@@ -51,6 +61,8 @@ public class ArticleListServlet extends HttpServlet {
 			List<Map<String, Object>> articleListMap = DBUtil.selectRows(conn, sql);
 			
 			request.setAttribute("page", page);
+			request.setAttribute("from", from);
+			request.setAttribute("end", end);
 			request.setAttribute("totalPage", totalPage);
 			request.setAttribute("articleListMap", articleListMap);
 			

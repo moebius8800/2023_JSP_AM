dd2c41f10791fc4d0a2427dc990db14ca94ee3eb
moebius8800 <moebius8800@gmail.com>
2023-04-07 15:42:53 +0900

    DB접속정보 분리

--- src/main/java/com/koreaIT/java/am/ArticleListServlet.java
+++ src/main/java/com/koreaIT/java/am/ArticleListServlet.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
+import com.koreaIT.java.am.config.Config;
 import com.koreaIT.java.am.util.DBUtil;
 import com.koreaIT.java.am.util.SecSql;
 
@@ -25,10 +26,9 @@ public class ArticleListServlet extends HttpServlet {
 		Connection conn = null;
 
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			String url = "jdbc:mysql://127.0.0.1:3306/JSPTest?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+			Class.forName(Config.getDBDriverName());
 
-			conn = DriverManager.getConnection(url, "root", "");
+			conn = DriverManager.getConnection(Config.getDBUrl(), Config.getDBUser(), Config.getDBPassWd());
 			
 			int page = 1;
 			if (request.getParameter("page") != null && request.getParameter("page").length() != 0) {
